f9f23b60b98dc2ba9eeb90040066233530fdc9a2
Riku-smile <smilesnoh@gmail.com>
2022-03-24 00:08:27 +0900

    feat: helpログを作成

--- cmd/root.go
+++ cmd/root.go
@@ -30,13 +30,11 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-timer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "ストップウォッチの機能を提供しています。",
+	Long: `ストップウォッチの機能を提供しています。
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	go-timer stopwatch で利用可能です。
+	終了時はCtrl+Cを入力してください。`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
